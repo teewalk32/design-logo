d360e257e3aaf420b696e85ee8dface1e1694287
teewalk32 <teewalk4321@gmail.com>
2023-05-02 21:17:21 -0500

    added prompts to js

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
-// TODO: Include packages needed for this application
+const shape = new Triangle();
+shape.setColor("blue");
+expect(shape.render()).toEqual('<polygon points="150, 18 244, 182 56, 182" fill="blue" />');
 
-// TODO: Create an array of questions for user input
-const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'Text',
+      message: 'Enter up to three characters',
+    },
+    {
+      type: 'input',
+      name: 'Text color',
+      message: 'Pick a text color',
+    },
+    {
+      type: 'input',
+      name: 'Shape',
+      message: 'Choose a shape',
+      choices: 'Circle, Triangle, and Square',
+    },
+    {
+      type: 'input',
+      name: 'Shape color',
+      message: 'Pick a color for the shapes',
+      choices: 'Blue, Red, Purple'
+    },
+])
+.then((answers) => {
+  const htmlPageContent = generateHTML(answers);
 
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+  fs.writeFile('index.html', htmlPageContent, (err) =>
+    err ? console.log(err) : console.log('Successfully created index.html!')
+  );
+});
